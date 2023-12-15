584259028e63d57bddfe3f333444b4e5f3fe3074
ericoandre <ericoandresilva@gmail.com>
2023-12-15 18:46:45 -0300

    inicial back-end

---
+++ Back-end/src/main/java/br/com/eccosystem/eccosystem/EccosystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.eccosystem.eccosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccosystemApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/br/com/eccosystem/eccosystem/EccosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eccosystem.eccosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
